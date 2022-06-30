8b2d9c61c6321c2b67c320e4d8db497074724e3e
Danait Kibrom <danaitkibrom@Danaits-MacBook-Pro.local>
2022-06-30 21:01:17 +0100

    getGradeSwitch

--- session-5/index.js
+++ session-5/index.js
@@ -1,37 +1,84 @@
-function getGrade(total) {
+// function getGrade(total) {
 
-let message = "Your grade is ";
+// let message = "Your grade is ";
 
-if (total > 90) {
-    message = "Your grade is A+ "
-}
-else  if (total > 80 && total <=90 ) {
-    message = "Your grade is A "
-}
-else if (total > 70 && total <=80 ) {
-    message = "Your grade is B "
-}
-else if (total > 60 && total <=70 ) {
-    message = "Your grade is C "
-}
-else if (total > 50 && total <=60 ) {
-    message = "Your grade is D "
-}
-else if (total > 40 && total <=50 ) {
-    message = "Your grade is E "
-}
-else if (total > 30 && total <=40 ) {
+// if (total > 90) {
+//     message = "Your grade is A+ "
+// }
+// else  if (total > 80 && total <=90 ) {
+//     message = "Your grade is A "
+// }
+// else if (total > 70 && total <=80 ) {
+//     message = "Your grade is B "
+// }
+// else if (total > 60 && total <=70 ) {
+//     message = "Your grade is C "
+// }
+// else if (total > 50 && total <=60 ) {
+//     message = "Your grade is D "
+// }
+// else if (total > 40 && total <=50 ) {
+//     message = "Your grade is E "
+// }
+// else if (total > 30 && total <=40 ) {
+//         message = "Your grade is F "
+// }
+// else if (total > 20 && total <=30 ) {
+//     message = " Fail "
+// }
+// else {
+//     message = " Unable to identify grade "
+// }
+//     return message;
+// }
+
+// // console.log(getGrade(Total))
+
+// console.log(getGrade(55))
+
+function getGradeSwitch(total) {
+    let message = "Your grade is";
+
+    switch (total > 0) {
+        case total === 90:
+        message = "Your grade is A+ "
+        break;
+
+        case total > 80 && age <=90:
+        message = "Your grade is A "
+        break;
+
+        case total > 70 && total <=80:
+        message = "Your grade is B "
+        break;
+
+        case total > 60 && total <=70:
+        message = "Your grade is C "
+        break;
+
+        case total > 50 && total <=60:
+        message = "Your grade is D "
+        break;
+
+        case total > 40 && total <=50:
+        message = "Your grade is E "
+        break;
+
+        case total > 30 && total <=40:
         message = "Your grade is F "
-}
-else if (total > 20 && total <=30 ) {
-    message = " Fail "
-}
-else {
-    message = " Unable to grade "
-}
-    return message;
-}
+        break;
+    
+        case total > 20 && total <=30:
+        message = " Fail "
+        break;
 
-// console.log(getGrade(Total))
-
-console.log(getGrade(55))
+    default:
+        message = " Unable to grade "
+        break;
+    }
+        return message;
+    }
+    
+    // console.log(getGrade(Total))
+    
+    console.log("getGradeSwitch", getGradeSwitch(55))
