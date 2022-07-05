67d0f46cef2e6423d62904b1e8f3ae06e7cc6642
Danait Kibrom <danaitkibrom@Danaits-MacBook-Pro.local>
2022-07-05 20:08:48 +0100

    Random Number

--- session-6/index.js
+++ session-6/index.js
@@ -1,13 +1,28 @@
 // Use this randomNumber inside of the while/for loops to generate a random number
-const randomNumber = Math.round(Math.random() * 10);
+// const randomNumber = Math.round(Math.random() * 10);
 
 // ****************************************************TASK 1****************************************************
 
+// let count = 0
+// while(count <= 10) {
+//     let randomNumber = Math.round(Math.random() * 10);
+//     count = count + 1;
 
+//     console.log('In the while ' + randomNumber);
 
+// }
 
+// console.log('Random number is: ' + randomNumber);
 
+let count = 0;
+let randomNumber = Math.round(Math.random() * 10);
 
+for (let count; count < 10; count++) {
+    randomNumber = randomNumber + 1
+    count = count + 1;
+}
+
+console.log('For was here ' + randomNumber + ' times! ');
 
 
 
